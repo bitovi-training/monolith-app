3b3594df317a92b755eba7953064f20538a67c27
Nicole Greene <nicole@bitovi.com>
2026-04-10 13:43:15 -0700

    remove go files

--- src/orders/go-implementation-archived/internal/models/product.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// ProductInfo represents product information from the Product Service
-type ProductInfo struct {
-	ID    string  `json:"id"`
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
-}
